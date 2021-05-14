85ebcb1df356080efaa149d238e3b95fc86d86c9
Linda Lai <lindalai.615@gmail.com>
2021-05-14 14:56:50 +1000

    refactor: Separate data loading, search and display, call from run()

--- index.js
+++ index.js
@@ -1,37 +1,127 @@
 const fs = require("fs");
 const readlineSync = require("readline-sync");
 
-const data = {
-  tickets: JSON.parse(fs.readFileSync("./data/tickets.json")),
-  organizations: JSON.parse(fs.readFileSync("./data/organizations.json")),
-  users: JSON.parse(fs.readFileSync("./data/users.json")),
+// const data = {
+//   tickets: JSON.parse(fs.readFileSync("./data/tickets.json")),
+//   organizations: JSON.parse(fs.readFileSync("./data/organizations.json")),
+//   users: JSON.parse(fs.readFileSync("./data/users.json")),
+// };
+
+// loadData()
+const loadData = () => {
+  return {
+    tickets: JSON.parse(fs.readFileSync("./data/tickets.json")),
+    organizations: JSON.parse(fs.readFileSync("./data/organizations.json")),
+    users: JSON.parse(fs.readFileSync("./data/users.json")),
+  };
 };
 
-const entityName = readlineSync.question(
-  "Which entity would you like to search? "
-);
-const field = readlineSync.question("Which field would you like to search? ");
-const value = readlineSync.question("Which value would you like to search? ");
+// promptUser()
+const promptUser = (data) => {
+  const entityName = readlineSync.question(
+    "Which entity would you like to search? "
+  );
+  const field = readlineSync.question("Which field would you like to search? ");
+  const value = readlineSync.question("Which value would you like to search? ");
 
-if (!data[entityName]) {
-  console.log("entity does not exist");
-  process.exit(1);
-}
+  if (!data[entityName]) {
+    console.log("entity does not exist");
+    process.exit(1);
+  }
 
-const search = (entityName, field, value) =>
-  data[entityName].filter((item) => item[field] === value);
+  return {
+    entityName,
+    field,
+    value,
+  };
+};
 
-const results = search(entityName, field, value);
+// const entityName = readlineSync.question(
+//   "Which entity would you like to search? "
+// );
+// const field = readlineSync.question("Which field would you like to search? ");
+// const value = readlineSync.question("Which value would you like to search? ");
 
-const border = () => console.log("=".repeat(100));
+// if (!data[entityName]) {
+//   console.log("entity does not exist");
+//   process.exit(1);
+// }
 
-border();
-console.log(
-  `Searched ${entityName.toUpperCase()} for "${field.toUpperCase()}" field and "${value.toUpperCase()}" value. Results: ${
-    results.length
-  }`
-);
-border();
+// // searchEntities()
+// const search = (entityName, field, value) =>
+//   data[entityName].filter((item) => item[field] === value);
+
+// const results = search(entityName, field, value);
+
+const search = (entityName, field, value, data) => {
+  // const { entityName, field, value } = inputs;
+
+  return data[entityName].filter((item) => item[field] === value);
+};
+
+// printResults()
+
+// TODO:
+const printResults = (inputs, results, data) => {
+  const { entityName, field, value } = inputs;
+  const border = () => console.log("=".repeat(100));
+
+  border();
+  console.log(
+    `Searched ${entityName.toUpperCase()} for "${field.toUpperCase()}" field and "${value.toUpperCase()}" value. Results: ${
+      results.length
+    }`
+  );
+  border();
+
+  results.forEach((entity) => {
+    printRecord(entityName, entity);
+    border();
+
+    if (entityName === "tickets") {
+      const [user] = search("users", "_id", entity.assignee_id, data);
+      const [organization] = search(
+        "organizations",
+        "_id",
+        entity.organization_id,
+        data
+      );
+      printRecord("user", user);
+      printRecord("organization", organization);
+    }
+
+    if (entityName === "users") {
+      const [organization] = search(
+        "organizations",
+        "_id",
+        entity.organization_id,
+        data
+      );
+      printRecord("organization", organization);
+
+      const tickets = search("tickets", "assignee_id", entity._id, data);
+      printRecords("ticket", tickets);
+    }
+
+    if (entityName === "organizations") {
+      const tickets = search("tickets", "organization_id", entity._id, data);
+      printRecords("ticket", tickets);
+
+      const users = search("users", "organization_id", entity._id, data);
+      printRecords("user", users);
+    }
+  });
+};
+
+// const border = () => console.log("=".repeat(100));
+
+// border();
+// console.log(
+//   `Searched ${entityName.toUpperCase()} for "${field.toUpperCase()}" field and "${value.toUpperCase()}" value. Results: ${
+//     results.length
+//   }`
+// );
+// border();
 
 const printRecord = (type, record) => {
   console.log(`----------- ${type} -----------`);
@@ -44,38 +134,20 @@ const printRecord = (type, record) => {
 const printRecords = (entityName, records) =>
   records.forEach((record) => printRecord(entityName, record));
 
-results.forEach((entity) => {
-  printRecord(entityName, entity);
-  border();
+function run() {
+  const data = loadData();
+  const inputs = promptUser(data);
+  const { entityName, field, value } = inputs;
+  const results = search(entityName, field, value, data);
+  console.log(results);
+  printResults(inputs, results, data);
+}
 
-  if (entityName === "tickets") {
-    const [user] = search("users", "_id", entity.assignee_id);
-    const [organization] = search(
-      "organizations",
-      "_id",
-      entity.organization_id
-    );
-    printRecord("user", user);
-    printRecord("organization", organization);
-  }
+// function run() {
+//   const data = loadData();
+//   const searchInputs = promptUser();
+//   const results = searchEntities(inputs, data);
+//   printResults(results);
+// }
 
-  if (entityName === "users") {
-    const [organization] = search(
-      "organizations",
-      "_id",
-      entity.organization_id
-    );
-    printRecord("organization", organization);
-
-    const tickets = search("tickets", "assignee_id", entity._id);
-    printRecords("ticket", tickets);
-  }
-
-  if (entityName === "organizations") {
-    const tickets = search("tickets", "organization_id", entity._id);
-    printRecords("ticket", tickets);
-
-    const users = search("users", "organization_id", entity._id);
-    printRecords("user", users);
-  }
-});
+run();
