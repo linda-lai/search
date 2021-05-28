dc0b72e33fc53f2e1757a57995b99d525b36b000
Linda Lai <lindalai.615@gmail.com>
2021-05-28 12:32:58 +1000

    test: Mock query in index.js

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const { search } = require("./search");
 const { loadData } = require("./data");
 
 const data = loadData();
-const query = promptUser(data);
+// const query = promptUser(data);
+
+const query = {
+  entityName: "tickets",
+  field: "_id",
+  value: "50f3fdbd-f8a6-481d-9bf7-572972856628",
+};
+
 const { entityName, field, value } = query;
+
 const results = search(entityName, field, value, data);
 printResults(query, results, data);
